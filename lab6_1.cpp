ce77fb6ab617ca10b2e2f48f84f3d00733f5adc0
guykamty <guykamty@gmail.com>
2024-12-02 23:29:15 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+int main() {
+    int i = 1, j, a = 0, b = 0;
+
+    while (i == 1) {
+        cout << "Enter an integer: ";
+        cin >> j;
+
+        if (j == 0) {
+            break;
+        } else if (j % 2 == 1) {
+            b++;
+        } else { 
+            a++;
+        }
+    }
+
+    cout << "#Even numbers = " << a << endl;
+    cout << "#Odd numbers = " << b << endl;
+
     return 0;
 }
